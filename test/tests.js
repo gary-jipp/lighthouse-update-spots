662c2c5a2712b13fcae554bb41f492dcfb90064d
Gary J <github@accu.ca>
2021-09-08 14:01:48 -0700

    Update

--- test/tests.js
+++ test/tests.js
@@ -15,19 +15,9 @@ describe('Update Spots Tests', function () {
     assert.equal(days[0].spots, 2);
   });
 
-  it('should update spots and old mutate the original day spots', function () {
-    assert.equal(days[0].spots, 2);
-    assert.equal(state.days[0].spots, oldState.days[0].spots);
-  });
-
   it('should update spots and not change original days array', function () {
     assert.equal(days[0].spots, 2);
     assert.equal(state.days, oldState.days);
-  });
-
-  it('should update spots and  not change the original day object', function () {
-    assert.equal(days[0].spots, 2);
-    assert.deepEqual(state.days[0], oldState.days[0]);
   });
 
 });
